6bf40361906e699edb9b81958bc94d3498a760b7
Josh <josh@joshthomas.dev>
2024-06-17 16:20:36 -0500

    temp remove yadm

--- build/install-packages.sh
+++ build/install-packages.sh
@@ -2,11 +2,9 @@
 
 set -ouex pipefail
 
-curl -Lo /etc/yum.repos.d/yadm.repo https://download.opensuse.org/repositories/home:TheLocehiliosan:yadm/Fedora_"${FEDORA_MAJOR_VERSION}"/home:TheLocehiliosan:yadm.repo
 curl -Lo /etc/yum.repos.d/wezterm-nightly.repo https://copr.fedorainfracloud.org/coprs/wezfurlong/wezterm-nightly/repo/fedora-"${FEDORA_MAJOR_VERSION}"/wezfurlong-wezterm-nightly-fedora-"${FEDORA_MAJOR_VERSION}".repo
 
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/1password.repo
-sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/yadm.repo
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/vivaldi.repo
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/wezterm-nightly.repo
 
@@ -14,10 +12,8 @@ rpm-ostree install \
         1password \
         1password-cli \
         vivaldi \
-        wezterm \
-        yadm
+        wezterm
 
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/1password.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/yadm.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/vivaldi.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/wezterm-nightly.repo
