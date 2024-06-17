0daa13a6e4d35b1a816cff22f98fbbc25a7eb4f6
Josh <josh@joshthomas.dev>
2024-06-17 16:34:42 -0500

    weeee

--- build/install-packages.sh
+++ build/install-packages.sh
@@ -8,11 +8,14 @@ sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/1password.repo
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/vivaldi.repo
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/wezterm-nightly.repo
 
-rpm-ostree install \
-        1password \
-        1password-cli \
-        vivaldi \
-        wezterm
+rpm-ostree install 1password
+echo 'hello after 1pass'
+rpm-ostree install 1password-cli
+echo 'hello after op'
+rpm-ostree install vivaldi
+echo 'hello after vivaldi'
+rpm-ostree install wezterm
+echo 'hello after wezterm'
 
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/1password.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/vivaldi.repo
