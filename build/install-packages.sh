37ccebf2ae96ae126e4eecb2e8071481a797ecc2
Josh <josh@joshthomas.dev>
2024-06-17 16:58:04 -0500

    disable op for now

--- build/install-packages.sh
+++ build/install-packages.sh
@@ -2,23 +2,23 @@
 
 set -ouex pipefail
 
-curl -Lo /etc/pki/rpm-gpg/1password.asc https://downloads.1password.com/linux/keys/1password.asc
+# curl -Lo /etc/pki/rpm-gpg/1password.asc https://downloads.1password.com/linux/keys/1password.asc
 curl -Lo /etc/yum.repos.d/wezterm-nightly.repo https://copr.fedorainfracloud.org/coprs/wezfurlong/wezterm-nightly/repo/fedora-"${FEDORA_MAJOR_VERSION}"/wezfurlong-wezterm-nightly-fedora-"${FEDORA_MAJOR_VERSION}".repo
 
-sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/1password.repo
+# sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/1password.repo
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/vivaldi.repo
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/wezterm-nightly.repo
 
-rpm-ostree install 1password
-echo 'hello after 1pass'
-rpm-ostree install 1password-cli
-echo 'hello after op'
+# rpm-ostree install 1password
+# echo 'hello after 1pass'
+# rpm-ostree install 1password-cli
+# echo 'hello after op'
 rpm-ostree install vivaldi
 echo 'hello after vivaldi'
 rpm-ostree install wezterm
 echo 'hello after wezterm'
 
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/1password.repo
+# sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/1password.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/vivaldi.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/wezterm-nightly.repo
 
