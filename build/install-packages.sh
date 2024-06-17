7b31108a678a1ae36176002c7f78852e1e7b51a6
Josh <josh@joshthomas.dev>
2024-06-17 16:50:46 -0500

    download op gpgkey

--- build/install-packages.sh
+++ build/install-packages.sh
@@ -2,6 +2,7 @@
 
 set -ouex pipefail
 
+curl -Lo /etc/pki/rpm-gpg/1password.asc https://downloads.1password.com/linux/keys/1password.asc
 curl -Lo /etc/yum.repos.d/wezterm-nightly.repo https://copr.fedorainfracloud.org/coprs/wezfurlong/wezterm-nightly/repo/fedora-"${FEDORA_MAJOR_VERSION}"/wezfurlong-wezterm-nightly-fedora-"${FEDORA_MAJOR_VERSION}".repo
 
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/1password.repo
