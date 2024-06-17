67286dd17b086f6c57e9b2eb31d1074559cd4660
Josh <josh@joshthomas.dev>
2024-06-17 17:04:34 -0500

    disable vivaldi too

--- build/install-packages.sh
+++ build/install-packages.sh
@@ -6,20 +6,20 @@ set -ouex pipefail
 curl -Lo /etc/yum.repos.d/wezterm-nightly.repo https://copr.fedorainfracloud.org/coprs/wezfurlong/wezterm-nightly/repo/fedora-"${FEDORA_MAJOR_VERSION}"/wezfurlong-wezterm-nightly-fedora-"${FEDORA_MAJOR_VERSION}".repo
 
 # sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/1password.repo
-sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/vivaldi.repo
+# sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/vivaldi.repo
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/wezterm-nightly.repo
 
 # rpm-ostree install 1password
 # echo 'hello after 1pass'
 # rpm-ostree install 1password-cli
 # echo 'hello after op'
-rpm-ostree install vivaldi
-echo 'hello after vivaldi'
+# rpm-ostree install vivali
+# echo 'hello after vivaldi'
 rpm-ostree install wezterm
 echo 'hello after wezterm'
 
 # sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/1password.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/vivaldi.repo
+# sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/vivaldi.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/wezterm-nightly.repo
 
 echo 'HELLOOOOOOOOOOOO'
