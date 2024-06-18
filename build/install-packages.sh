bf7c65b8da110379b988648baf36df251182d36a
Josh <josh@joshthomas.dev>
2024-06-17 22:26:28 -0500

    try this again

--- build/install-packages.sh
+++ build/install-packages.sh
@@ -3,6 +3,7 @@
 set -ouex pipefail
 
 # curl -Lo /etc/pki/rpm-gpg/1password.asc https://downloads.1password.com/linux/keys/1password.asc
+curl -Lo /etc/pki/rpm-gpg/RPM-GPG-KEY-vivaldi https://repo.vivaldi.com/archive/linux_signing_key.pub
 curl -Lo /etc/yum.repos.d/wezterm-nightly.repo https://copr.fedorainfracloud.org/coprs/wezfurlong/wezterm-nightly/repo/fedora-"${FEDORA_MAJOR_VERSION}"/wezfurlong-wezterm-nightly-fedora-"${FEDORA_MAJOR_VERSION}".repo
 
 # sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/1password.repo
@@ -11,7 +12,6 @@ sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/wezterm-nightly.repo
 
 # rpm-ostree install 1password
 # rpm-ostree install 1password-cli
-# rpm-ostree install vivali
 # NOTES:
 # - zlib-ng-compat & zlib-ng-compat-devel:
 #   needed for python installation on Fedora 40
